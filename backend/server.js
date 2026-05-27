6e564b6fbfb4fd3eca96d12dabccd2fd383c5a4f
Divyansh9917 <divyanshm027@gmail.com>
2026-05-27 15:47:14 +0530

    proxy fix

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
 // 1. HTTP Server wrap for Express (Required for Socket.io)
 const server = http.createServer(app);
 
